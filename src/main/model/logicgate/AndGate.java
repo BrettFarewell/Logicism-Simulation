243a47def43f5e587a74036e75aba99202c5f873
Brett Farewell <brettfar@students.cs.ubc.ca>
2025-02-13 23:04:18 -0800

    Added specifiatcions for AndGate class

--- src/main/model/logicgate/AndGate.java
+++ src/main/model/logicgate/AndGate.java
@@ -1,6 +1,24 @@
 package model.logicgate;
 
 public class AndGate implements LogicGate {
-    
 
+    // EFFECTS: creates AND Gate, with left and botton input values set to "off"
+    public AndGate() {
+        // STUB
+    }
+    
+    // MODIFIES: this
+    // EFFECTS: takes input direct, if direction is "left", switch left input to "on"
+    //                              if direction is "bot", switch bottom input to "on"
+    //                              return result of output()
+    @Override
+    public boolean input(String dir) {
+        return false; // STUB
+    }
+
+    // EFFECTS: if both left and bottom inputs are "on", return true
+    @Override
+    public boolean output() {
+        return false; // STUB
+    }
 }
