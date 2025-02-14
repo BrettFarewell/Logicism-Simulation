273cd34046cb3c212609c56dca19575fd12a849f
Brett Farewell <brettfar@students.cs.ubc.ca>
2025-02-14 00:06:01 -0800

    Updated specifications of Wire class

--- src/main/model/Wire.java
+++ src/main/model/Wire.java
@@ -1,14 +1,15 @@
 package model;
 
-// Represents a wire objects that will propagate any source signal given
+// Represents a wire objects that will propagate any source signal given, with x and y positions in scenario, and 
+// "on" or "off" power status
 public class Wire {
     
-    // EFFECTS: creates a wire with output set to "off" and x and y positions set to given x and y values
+    // EFFECTS: creates a wire with power status set to "off" and x and y positions set to given x and y values
     public Wire(int x, int y) {
 
     }
 
-    // EFFECT: set output to "on" and return true
+    // EFFECT: set power status to "on" and return true
     public boolean output() {
         return false; // STUB
     }
