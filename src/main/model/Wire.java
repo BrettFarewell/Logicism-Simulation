e5b531bd8cfced3bb33439f7f59e8dfab7c7cddb
Brett Farewell <brettfar@students.cs.ubc.ca>
2025-02-14 13:41:09 -0800

    Changed Wire class to extend LogicElement abstract class and updates specifications

--- src/main/model/Wire.java
+++ src/main/model/Wire.java
@@ -1,31 +1,19 @@
 package model;
 
 // Represents a wire objects that will propagate any source signal given, with x and y positions in scenario, and 
-// "on" or "off" power status
-public class Wire {
+// "on" or "off" power status.
+public class Wire extends LogicElement {
     
     // EFFECTS: creates a wire with power status set to "off" and x and y positions set to given x and y values
-    public Wire(int x, int y) {
-
-    }
-
-    // EFFECT: set power status to "on"
-    public void input() {
+    //          and sets logic elements to the left, right, above and below
+    public Wire(int x, int y, LogicElement left, LogicElement right, LogicElement above, LogicElement below) {
         // STUB
     }
 
-    // return power status of wire
-    public boolean getPowerStatus() {
-        return false; // STUB
-    }
-
-    // EFFECTS: gets x position
-    public int getPosX() {
-        return -1; // STUB
-    }
-
-    // EFFECTS: gets y position
-    public int getPosY() {
-        return -1; // STUB
+    // MODIFIES: this
+    // EFFECTS: checks to see if any input are "on", if so turns power status "on" and calls outputLeft(),
+    //          outputRight(), outputAbove(), outputBelow(),
+    public void checkPowerStatus() {
+        // STUB
     }
 }
