d5b583c8c7ee9261002628777b5e5d23360f7b40
Brett Farewell <brettfar@students.cs.ubc.ca>
2025-02-13 23:40:14 -0800

    Added temporary specifications to Wire class

--- src/main/model/Wire.java
+++ src/main/model/Wire.java
@@ -1,5 +1,15 @@
 package model;
 
+// Represents a wire objects that will propagate any source signal given
 public class Wire {
     
+    // EFFECTS: creates a wire with output set to "off" and x and y positions set to given x and y values
+    public Wire(int x, int y) {
+
+    }
+
+    // EFFECT: set output to "on" and return true
+    public boolean output() {
+        return false; // STUB
+    }
 }
