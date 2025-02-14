b20ebabb523ec1db34dffe76f85ae0c325aa7ee9
Brett Farewell <brettfar@students.cs.ubc.ca>
2025-02-13 23:44:46 -0800

    Added specifications to Main class

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -1,5 +1,6 @@
 package ui;
 
+// Represents where the program will be started from
 public class Main {
     public static void main(String[] args) throws Exception {
         System.out.println("Welcome to my project!");
