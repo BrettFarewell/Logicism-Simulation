be1794c7d2c03476266c81988d75829745d3fee5
Brett Farewell <brettfar@students.cs.ubc.ca>
2025-02-14 13:52:52 -0800

    Changed PowerSource class to extend LogicElement and updated specifications

--- src/main/model/PowerSource.java
+++ src/main/model/PowerSource.java
@@ -1,26 +1,23 @@
 package model;
 
 // Represents a power sourse where power will start from. Will contain x and y positions
-public class PowerSource {
+public class PowerSource extends LogicElement {
     private static final boolean POWER_STATUS = true;
     
-    // creates a power source where power will start from, sets x and y positions
-    public PowerSource(int x, int y) {
+    // EFFECTS: creates a power source where power will start from, sets x and y positions, and set logic elements to the
+    //          left, right, above and below. 
+    public PowerSource(int x, int y, LogicElement left, LogicElement right, LogicElement above, LogicElement below) {
+        // STUB
+    }
+
+    // EFFECT: propagates the signal to logic elements to the left, right, above, and below
+    public void checkPowerStatus() {
         // STUB
     }
 
     // EFFECTS: returns POWER_STATUS
+    @Override
     public boolean getPowerStatus() {
-        return false; // STUB
-    }
-    
-    // EFFECTS: gets x position
-    public int getPosX() {
-        return -1; // STUB
-    }
-
-    // EFFECTS: gets y position
-    public int getPosY() {
-        return -1; // STUB
+        return POWER_STATUS; // STUB
     }
 }
