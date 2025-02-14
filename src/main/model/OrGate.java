e1034ff40c4e52543c9b4e993b9e0b351c864f96
Brett Farewell <brettfar@students.cs.ubc.ca>
2025-02-14 13:35:22 -0800

    Updated specifications of OrGateand AndGate classes

--- src/main/model/OrGate.java
+++ src/main/model/OrGate.java
@@ -4,6 +4,11 @@ package model;
 // and an x and an y position.
 public class OrGate extends LogicElement  {
 
+    // Creates an Or Gate at given x and y coordinates, and set logic elements to the left, right, above and below
+    public OrGate(int x, int y, LogicElement left, LogicElement right, LogicElement above, LogicElement below) {
+        // STUB
+    }
+
     // MODIFIES: this
     // EFFECTS: checks to see if left or bottom inputs are "on", if so turns power status "on" and calls outputRight()
     public void checkPowerStatus() {
