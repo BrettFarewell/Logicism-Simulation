1954a290ecefd0e3932746664a7acbcf5bff1cd8
Brett Farewell <brettfar@students.cs.ubc.ca>
2025-02-14 00:11:08 -0800

    Added specifications to PowerSource class

---
+++ src/main/model/PowerSource.java
@@ -0,0 +1,26 @@
+package model;
+
+// Represents a power sourse where power will start from. Will contain x and y positions
+public class PowerSource {
+    private final boolean POWER_STATUS = true;
+    
+    // creates a power source where power will start from, sets x and y positions
+    public PowerSource(int x, int y) {
+        // STUB
+    }
+
+    // EFFECTS: returns POWER_STATUS
+    public boolean output() {
+        return false; // STUB
+    }
+    
+    // EFFECTS: gets x position
+    public int getPosX() {
+        return -1; // STUB
+    }
+
+    // EFFECTS: gets y position
+    public int getPosY() {
+        return -1; // STUB
+    }
+}
