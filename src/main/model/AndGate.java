e0b8c65eb856dd8f40bc8513b20d9538d44f6d96
Brett Farewell <brettfar@students.cs.ubc.ca>
2025-02-14 13:27:39 -0800

    Changed AndGate class to extend LogicElement abstract class and updated specifications

---
+++ src/main/model/AndGate.java
@@ -0,0 +1,12 @@
+package model;
+
+// Respresents the AND Gate logic gate that has a left and a bottom input, an power status, a right output,
+// and an x and an y position.
+public class AndGate extends LogicElement {
+
+    // MODIFIES: this
+    // EFFECTS: checks to see if left and bottom inputs are "on", if so turns power status "on" and calls outputRight()
+    public void checkPowerStatus() {
+        // STUIB
+    }
+}
