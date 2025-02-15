b8675d40089846fefd0d6de770c15544b81917c6
Brett Farewell <brettfar@students.cs.ubc.ca>
2025-02-15 13:07:56 -0800

    Updated specifcications to LightOutput class and OutputElement abstract class

--- src/main/model/OutputElement.java
+++ src/main/model/OutputElement.java
@@ -3,4 +3,27 @@ package model;
 // Represents all output elements. Output elements contain power inputs in all directions but no power outputs
 public abstract class OutputElement extends LogicElement {
     
+    // EFFECTS: should not be called, but if so, does nothing and stops propagation of output to the left
+    @Override
+    public void outputLeft() {
+        // STUB
+    }
+
+    // EFFECTS: should not be called, but if so, does nothing and stops propagation of output to the right
+    @Override
+    public void outputRight() {
+        // STUB
+    }
+
+    // EFFECTS: should not be called, but if so, does nothing and stops propagation of output to above
+    @Override
+    public void outputAbove() {
+        // STUB
+    }
+
+    // EFFECTS: should not be called, but if so, does nothing and stops propagation of output to below
+    @Override
+    public void outputBelow() {
+        // STUB
+    }
 }
