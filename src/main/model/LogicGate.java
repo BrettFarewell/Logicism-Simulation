97196374c39973d78fa83507df62ade5f693c93f
Brett Farewell <brettfar@students.cs.ubc.ca>
2025-02-15 13:08:32 -0800

    Updated specifications to LogicGate abstract class

--- src/main/model/LogicGate.java
+++ src/main/model/LogicGate.java
@@ -1,5 +1,6 @@
 package model;
 
+// Represents all Logic Gates. All Logic Gates will have have left and below inputs, as well as only a right output
 public abstract class LogicGate extends LogicElement {
     
 }
