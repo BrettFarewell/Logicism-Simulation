93fde5999454659531ea0901478d09f5d795a235
Brett Farewell <brettfar@students.cs.ubc.ca>
2025-02-15 02:57:03 -0800

    Updated specfications to OutputElement

--- src/main/model/OutputElement.java
+++ src/main/model/OutputElement.java
@@ -1,5 +1,6 @@
 package model;
 
+// Represents all output elements. Output elements contain power inputs in all directions but no power outputs
 public abstract class OutputElement extends LogicElement {
     
 }
