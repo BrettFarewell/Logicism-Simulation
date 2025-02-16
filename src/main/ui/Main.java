79f5d1f0294e9bdfdbbd27c4b801c5c54b474737
Brett Farewell <brettfar@students.cs.ubc.ca>
2025-02-15 23:04:59 -0800

    Added implementation to Main class

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -3,6 +3,6 @@ package ui;
 // Represents where the program will be started from
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println("Welcome to my project!");
+        StartingScreen startingScreen = new StartingScreen();
     }
 }
