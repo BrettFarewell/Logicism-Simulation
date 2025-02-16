bdd9d2a88966d1a7d3d607ed18d366bbf4a12cbb
Brett Farewell <brettfar@students.cs.ubc.ca>
2025-02-16 15:28:36 -0800

    Changed implementation on Main class

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -3,6 +3,6 @@ package ui;
 // Represents where the program will be started from
 public class Main {
     public static void main(String[] args) throws Exception {
-        StartingScreen startingScreen = new StartingScreen();
+        new StartingScreen();
     }
 }
