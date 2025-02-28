c1dd579df5512a071524d998d7d3bf827abc7c5d
Brett Farewell <brettfar@students.cs.ubc.ca>
2025-02-28 00:13:25 -0800

    Created classes JsonReader and JsonWriter, as well as Writable interface with specfications all in the persistance package

---
+++ src/main/persistance/JsonWriter.java
@@ -0,0 +1,47 @@
+package persistance;
+
+import model.Scenario;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
+import org.json.JSONObject;
+
+// 
+public class JsonWriter {
+    private static final int TAB = 4;
+    private PrintWriter writer;
+    private String destination;
+
+    // EFFECTS: constructs writer to write to destination file
+    public JsonWriter(String destination) {
+        this.destination = destination;
+    }
+
+    // MODIFIES: this
+    // EFFECTS: opens writer; throws FileNotFoundException if destination file cannot
+    // be opened for writing
+    public void open() throws FileNotFoundException {
+        writer = new PrintWriter(new File(destination));
+    }
+
+    // MODIFIES: this
+    // EFFECTS: writes JSON representation of workroom to file
+    public void write(Scenario scen) {
+        JSONObject json = scen.toJson();
+        saveToFile(json.toString(TAB));
+    }
+
+    // MODIFIES: this
+    // EFFECTS: closes writer
+    public void close() {
+        writer.close();
+    }
+
+    // MODIFIES: this
+    // EFFECTS: writes string to file
+    private void saveToFile(String json) {
+        writer.print(json);
+    }    
+}
---
+++ src/main/persistance/Writable.java
@@ -0,0 +1,10 @@
+package persistance;
+
+import org.json.JSONObject;
+
+// Represents a writable interface to allow objects to be written as JSON objects
+public interface Writable {
+    // ATTRIBUTION: Code structure based on JSONSerializationDemo
+    // EFFECTS: returns this as JSON object
+    JSONObject toJson();
+}
