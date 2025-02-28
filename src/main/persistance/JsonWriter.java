b4f23c527fa288f339bea80e3732763640d7c4c5
Brett Farewell <brettfar@students.cs.ubc.ca>
2025-02-28 00:56:53 -0800

    Added specifications for the JsonWrite class

--- src/main/persistance/JsonWriter.java
+++ src/main/persistance/JsonWriter.java
@@ -8,40 +8,45 @@ import java.io.PrintWriter;
 
 import org.json.JSONObject;
 
-// 
+// ATTRIBUTION: many of the methods in the class come from the JsonWriter class in the JSONSerializeationDemo
+// Represents a writer that writes JSON representation of CircuitBuilderState to file
 public class JsonWriter {
     private static final int TAB = 4;
     private PrintWriter writer;
     private String destination;
 
+    // ATTRIBUTION: Code structure based on JSONSerializationDemo
     // EFFECTS: constructs writer to write to destination file
     public JsonWriter(String destination) {
-        this.destination = destination;
+        // STUB
     }
 
+    // ATTRIBUTION: Code structure based on JSONSerializationDemo
     // MODIFIES: this
     // EFFECTS: opens writer; throws FileNotFoundException if destination file cannot
     // be opened for writing
     public void open() throws FileNotFoundException {
-        writer = new PrintWriter(new File(destination));
+        // STUB
     }
 
+    // ATTRIBUTION: Code structure based on JSONSerializationDemo
     // MODIFIES: this
     // EFFECTS: writes JSON representation of workroom to file
     public void write(Scenario scen) {
-        JSONObject json = scen.toJson();
-        saveToFile(json.toString(TAB));
+        // STUB
     }
 
+    // ATTRIBUTION: Code structure based on JSONSerializationDemo
     // MODIFIES: this
     // EFFECTS: closes writer
     public void close() {
-        writer.close();
+        // STUB
     }
 
+    // ATTRIBUTION: Code structure based on JSONSerializationDemo
     // MODIFIES: this
     // EFFECTS: writes string to file
     private void saveToFile(String json) {
-        writer.print(json);
+        // STUB
     }    
 }
