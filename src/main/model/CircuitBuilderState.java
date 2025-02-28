2e2c48427fb6ad526e349f0aad9a34ee4f0bf12c
Brett Farewell <brettfar@students.cs.ubc.ca>
2025-02-28 00:17:38 -0800

    Updated specifications for CircuitBuilderState class

--- src/main/model/CircuitBuilderState.java
+++ src/main/model/CircuitBuilderState.java
@@ -3,11 +3,17 @@ package model;
 import persistance.Writable;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import org.json.JSONObject;
 
 // Represents the state of the circuit builder application is currently in will all of the scenarios within
 public class CircuitBuilderState implements Writable {
+
+    // EFFECT: creates a CircuitBuilderState with an empty scenario list
+    public CircuitBuilderState() {
+
+    }
 
     // EFFECT: returns list of scenarios in the current state of the circuit builder 
     public ArrayList<Scenario> getScenarioList() {
