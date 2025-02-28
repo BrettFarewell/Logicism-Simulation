02305a8eddb23038e47e5c1560b9307619d1e65d
Brett Farewell <brettfar@students.cs.ubc.ca>
2025-02-28 00:14:13 -0800

    Created and added specifications for CircuitBuilderState class in model package

---
+++ src/main/model/CircuitBuilderState.java
@@ -0,0 +1,22 @@
+package model;
+
+import persistance.Writable;
+
+import java.util.ArrayList;
+
+import org.json.JSONObject;
+
+// Represents the state of the circuit builder application is currently in will all of the scenarios within
+public class CircuitBuilderState implements Writable {
+
+    // EFFECT: returns list of scenarios in the current state of the circuit builder 
+    public ArrayList<Scenario> getScenarioList() {
+        return null;
+    }
+
+    @Override
+    public JSONObject toJson() {
+        return null;
+    }
+    
+}
