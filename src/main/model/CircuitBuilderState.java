db470dca969bfb0dba630ea48d4e4bcc576d9df8
Brett Farewell <brettfar@students.cs.ubc.ca>
2025-02-28 00:44:26 -0800

    Added implementation and fixed tests for CircuitBuilderState class

--- src/main/model/CircuitBuilderState.java
+++ src/main/model/CircuitBuilderState.java
@@ -9,21 +9,22 @@ import org.json.JSONObject;
 
 // Represents the state of the circuit builder application is currently in will all of the scenarios within
 public class CircuitBuilderState implements Writable {
+    private List<Scenario> scenarios;
 
     // EFFECT: creates a CircuitBuilderState with an empty scenario list
     public CircuitBuilderState() {
-
+        scenarios = new ArrayList<Scenario>();
     }
 
     // MODIFIES: this
     // EFFECT: adds a scenario to the list of scenarios
     public void addScenario(Scenario scenario) {
-        
+        this.scenarios.add(scenario);
     }
 
     // EFFECT: returns list of scenarios in the current state of the circuit builder 
-    public ArrayList<Scenario> getScenarioList() {
-        return null;
+    public List<Scenario> getScenarioList() {
+        return this.scenarios;
     }
 
     @Override
--- src/test/model/CircuitBuilderStateTest.java
+++ src/test/model/CircuitBuilderStateTest.java
@@ -2,28 +2,28 @@ package model;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.Test;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class CircuitBuilderStateTest {
     private CircuitBuilderState circuitBuilderState;
-    Scenario scenario1;
-    Scenario scenario2;
+    private Scenario scenario1;
+    private Scenario scenario2;
 
     @BeforeEach
-    void setup() {
+    void runBefore() {
         circuitBuilderState = new CircuitBuilderState();
         scenario1 = new Scenario("Scenario1");
         scenario2 = new Scenario("Scenario2");
     }
 
     @Test
-    void testConstructor() {
+    void constructorTest() {
         assertTrue(circuitBuilderState.getScenarioList().isEmpty());
     }
 
     @Test
-    void testAddScenarios() {
+    void addScenariosTests() {
         circuitBuilderState.addScenario(scenario1);
         assertEquals(scenario1, circuitBuilderState.getScenarioList().get(0));
         circuitBuilderState.addScenario(scenario2);
