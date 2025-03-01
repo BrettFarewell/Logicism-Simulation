c0e6fdadcd34f3d3b3d233ebe2c75069bca42468
Brett Farewell <brettfar@students.cs.ubc.ca>
2025-02-28 19:52:51 -0800

    Added implementation to JsonReader class

--- src/main/persistence/JsonReader.java
+++ src/main/persistence/JsonReader.java
@@ -26,46 +26,70 @@ public class JsonReader {
     // EFFECTS: reads CircuitBuilderState from file and returns it;
     // throws IOException if an error occurs reading data from file
     public CircuitBuilderState read() throws IOException {
-        return null; // STUB
+        String jsonData = readFile(source);
+        JSONObject jsonObject = new JSONObject(jsonData);
+        return parseCircuitBuilderState(jsonObject);
     }
 
     // ATTRIBUTION: Code structure based on JSONSerializationDemo JsonReader class
     // EFFECTS: reads source file as string and returns it
     private String readFile(String source) throws IOException {
-        return null; // STUB
+        StringBuilder contentBuilder = new StringBuilder();
+
+        try (Stream<String> stream = Files.lines(Paths.get(source), StandardCharsets.UTF_8)) {
+            stream.forEach(s -> contentBuilder.append(s));
+        }
+
+        return contentBuilder.toString();
     }
 
     // ATTRIBUTION: Code structure based on JSONSerializationDemo JsonReader class
     // EFFECTS: parses CircuitBuilderState from JSON object and returns it
     private CircuitBuilderState parseCircuitBuilderState(JSONObject jsonObject) {
-        return null; // STUB
+        CircuitBuilderState circuitBuilderState = new CircuitBuilderState();
+        addScenarios(circuitBuilderState, jsonObject);
+        return circuitBuilderState;
     }
 
     // ATTRIBUTION: Code structure based on JSONSerializationDemo JsonReader class
     // MODIFIES: circuitBuilderState
     // EFFECTS: parses scenarios from JSON object and adds them to CircuitBuilderState
     private void addScenarios(CircuitBuilderState circuitBuilderState, JSONObject jsonObject) {
-        // STUB
+        JSONArray jsonArray = jsonObject.getJSONArray("scenarios");
+        for (Object json : jsonArray) {
+            JSONObject nextScenario = (JSONObject) json;
+            addScenario(circuitBuilderState, nextScenario);
+        }
     }
 
     // ATTRIBUTION: Code structure based on JSONSerializationDemo JsonReader class
     // MODIFIES: circuitBuilderState
     // EFFECTS: parses scenario from JSON object and adds it to CircuitBuilderState
     private void addScenario(CircuitBuilderState circuitBuilderState, JSONObject jsonObject) {
-        // STUB
+        String name = jsonObject.getString("name");
+        Scenario scenario = new Scenario(name);
+        addLogicElements(scenario, circuitBuilderState, jsonObject);
+        circuitBuilderState.addScenario(scenario);
     }
 
     // ATTRIBUTION: Code structure based on JSONSerializationDemo JsonReader class
-    // MODIFIES: circuitBuilderState
+    // MODIFIES: circuitBuilderState, scenario
     // EFFECTS: parses LogicElements from JSON object and adds it to CircuitBuilderState
-    private void addLogicElements(CircuitBuilderState circuitBuilderState, JSONObject jsonObject) {
-        // STUB
+    private void addLogicElements(Scenario scenario, CircuitBuilderState circuitBuilderState, JSONObject jsonObject) {
+        JSONArray jsonArray = jsonObject.getJSONArray("logicelements");
+        for (Object json : jsonArray) {
+            JSONObject nextScenario = (JSONObject) json;
+            addLogicElement(scenario, circuitBuilderState, nextScenario);
+        }
     }
 
     // ATTRIBUTION: Code structure based on JSONSerializationDemo JsonReader class
-    // MODIFIES: circuitBuilderState
+    // MODIFIES: circuitBuilderState, scenario
     // EFFECTS: parses LogicElement from JSON object and adds it to CircuitBuilderState
-    private void addLogicElement(CircuitBuilderState circuitBuilderState, JSONObject jsonObject) {
-        // STUB
+    private void addLogicElement(Scenario scenario, CircuitBuilderState circuitBuilderState, JSONObject jsonObject) {
+        int posX = jsonObject.getInt("posX");
+        int posY = jsonObject.getInt("posY");
+        String key = jsonObject.getString("category");
+        scenario.addLogicElement(key, posX, posY);
     }
 }
